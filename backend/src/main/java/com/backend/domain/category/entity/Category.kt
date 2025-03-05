fbb86a6156c217fef996457cbefd58989fb28723
Gonhub9 <ho_gok@naver.com>
2025-03-05 16:17:07 +0900

    style: import 정리

--- backend/src/main/java/com/backend/domain/category/entity/Category.kt
+++ backend/src/main/java/com/backend/domain/category/entity/Category.kt
@@ -1,24 +1,8 @@
-package com.backend.domain.category.entity;
+package com.backend.domain.category.entity
 
-import com.backend.domain.category.entity.QCategory.category
-import com.backend.domain.post.entity.Post;
-import com.backend.global.baseentity.BaseEntity;
-import jakarta.persistence.CascadeType;
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.OneToMany;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Objects;
-import lombok.AccessLevel;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import org.springframework.data.jpa.domain.AbstractPersistable_.id
+import com.backend.domain.post.entity.Post
+import com.backend.global.baseentity.BaseEntity
+import jakarta.persistence.*
 
 @Entity
 open class Category protected constructor() : BaseEntity() {
