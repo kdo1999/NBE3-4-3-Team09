81c9917a1de1a580b5e7a8e8928259a208cf5094
zelly-log <dearmyjaei@gmail.com>
2025-03-05 16:42:57 +0900

    refactor: Post 엔티티 코틀린으로 변환

--- backend/src/main/java/com/backend/domain/post/entity/Post.kt
+++ backend/src/main/java/com/backend/domain/post/entity/Post.kt
@@ -1,3 +1,53 @@
 package com.backend.domain.post.entity
 
-data class Post()
+import com.backend.domain.category.entity.Category
+import com.backend.domain.comment.entity.Comment
+import com.backend.domain.user.entity.SiteUser
+import com.backend.domain.voter.entity.Voter
+import com.backend.global.baseentity.BaseEntity
+import jakarta.persistence.*
+
+@Entity
+@Inheritance(strategy = InheritanceType.JOINED)
+@DiscriminatorColumn(name = "post_type")
+@Table(name = "post")
+open class Post( // 상속하려면 open class
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "post_id")
+    val postId: Long?= null,
+
+    @Column(nullable = false)
+    var subject: String,
+
+    @Column(nullable = false)
+    var content: String,
+
+    @ManyToOne
+    @JoinColumn(name = "category_id", nullable = false)
+    val category: Category,
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    val author: SiteUser
+) : BaseEntity() {
+
+    @OneToMany(fetch = FetchType.LAZY, mappedBy = "post", cascade = [CascadeType.REMOVE])
+    private var _postCommentList: MutableList<Comment> = mutableListOf()
+
+    val postCommentList: List<Comment>
+        get() = _postCommentList.toList()
+
+    @OneToMany(fetch = FetchType.LAZY, mappedBy = "post", cascade = [CascadeType.REMOVE])
+    private var _postVoterList: MutableList<Voter> = mutableListOf()
+
+    val postVoterList: List<Voter>
+        get() = _postVoterList.toList()
+
+
+    // 게시글 수정 메서드
+    fun updatePost(subject: String, content: String){
+        this.subject = subject
+        this.content = content
+    }
+}
--- backend/src/main/java/com/backend/domain/post/entity/RecruitmentPost.kt
+++ backend/src/main/java/com/backend/domain/post/entity/RecruitmentPost.kt
@@ -1,4 +1,39 @@
 package com.backend.domain.post.entity
 
-class RecruitmentPost {
+import com.backend.domain.category.entity.Category
+import com.backend.domain.jobposting.entity.JobPosting
+import com.backend.domain.user.entity.SiteUser
+import jakarta.persistence.*
+import java.time.ZonedDateTime
+
+@Entity
+@DiscriminatorValue("recruitment")
+open class RecruitmentPost (
+
+    postId: Long? = null,
+    subject: String,
+    content: String,
+    category: Category,
+    author: SiteUser,
+
+    private var recruitmentClosingDate: ZonedDateTime?,
+    private var numOfApplicants: Int?,
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = true)
+    var recruitmentStatus: RecruitmentStatus?,
+
+    @ManyToOne
+    @JoinColumn(name = "job_id", nullable = true)
+    var jobPosting: JobPosting?
+) : Post(postId, subject, content, category, author) {
+
+    fun updatePost(subject: String, content: String, numOfApplicants: Int?){
+        super.updatePost(subject, content)
+        this.numOfApplicants = numOfApplicants
+    }
+
+    fun updateRecruitmentStatus(recruitmentStatus: RecruitmentStatus){
+        this.recruitmentStatus = recruitmentStatus
+    }
 }
