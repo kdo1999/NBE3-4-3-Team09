26ae6af56d202eb9ae49e03ec729c08d0c0a0121
Gonhub9 <ho_gok@naver.com>
2025-03-05 17:15:01 +0900

    refactor: 파일명 변경 후 코틀린으로 변환

--- backend/src/main/java/com/backend/domain/category/dto/request/CategoryRequest.kt
+++ backend/src/main/java/com/backend/domain/category/dto/request/CategoryRequest.kt
@@ -1,16 +1,8 @@
-package com.backend.domain.category.dto.request;
+package com.backend.domain.category.dto.request
 
-import jakarta.validation.constraints.NotBlank;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import jakarta.validation.constraints.NotBlank
 
-@Builder
-@Getter
-@NoArgsConstructor
-@AllArgsConstructor
-public class CategoryRequest {
-    @NotBlank(message = "카테고리 이름을 입력해주세요.")
-    private String name;
-}
+data class CategoryRequest(
+    @field:NotBlank(message = "카테고리 이름을 입력해주세요.")
+    val name: String = ""
+)
