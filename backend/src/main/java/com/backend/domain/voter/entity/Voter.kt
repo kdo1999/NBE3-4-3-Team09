c707b51c03850fa16b1145c153ae489dbae1fdb6
Kim Dong O <kdo_1999@naver.com>
2025-03-05 11:42:16 +0900

    refactor: Voter 마이그레이션

--- backend/src/main/java/com/backend/domain/voter/entity/Voter.kt
+++ backend/src/main/java/com/backend/domain/voter/entity/Voter.kt
@@ -1,55 +1,50 @@
-package com.backend.domain.voter.entity;
+package com.backend.domain.voter.entity
 
-import com.backend.domain.jobposting.entity.JobPosting;
-import com.backend.domain.post.entity.Post;
-import com.backend.domain.user.entity.SiteUser;
-import com.backend.domain.voter.domain.VoterType;
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.EnumType;
-import jakarta.persistence.Enumerated;
-import jakarta.persistence.FetchType;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.Table;
-import lombok.AccessLevel;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import com.backend.domain.jobposting.entity.JobPosting
+import com.backend.domain.post.entity.Post
+import com.backend.domain.user.entity.SiteUser
+import com.backend.domain.voter.domain.VoterType
+import jakarta.persistence.*
 
 @Entity
 @Table(name = "voter")
-@NoArgsConstructor(access = AccessLevel.PROTECTED)
-@AllArgsConstructor(access = AccessLevel.PROTECTED)
-@Builder
-@Getter
-public class Voter {
-
+class Voter {
 	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	@Column(name = "voter_id")
-	private Long id;
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "voter_id")
+	var id: Long? = null
+	protected set
 
 	@ManyToOne(fetch = FetchType.LAZY)
-	@JoinColumn(name = "job_posting_id")
-	private JobPosting jobPosting;
+    @JoinColumn(name = "job_posting_id")
+	var jobPosting: JobPosting? = null
+	protected set
 
-	@ManyToOne(fetch = FetchType.LAZY)
-	@JoinColumn(name = "post_id")
-	private Post post;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "post_id")
+    var post: Post? = null
+	protected set
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "site_user_id")
+    lateinit var siteUser: SiteUser
+	protected set
+
+    @Enumerated(EnumType.STRING)
+    @Column(length = 20)
+    lateinit var voterType: VoterType
+	protected set
 
 
-	@ManyToOne(fetch = FetchType.LAZY)
-	@JoinColumn(name = "site_user_id")
-	private SiteUser siteUser;
+	constructor(jobPosting: JobPosting, siteUser: SiteUser, voterType: VoterType) {
+		this.jobPosting = jobPosting
+		this.siteUser = siteUser
+		this.voterType = voterType
+	}
 
-	@Enumerated(EnumType.STRING)
-	@Column(length = 20)
-	private VoterType voterType;
-
-	//TODO 추후 게시글 필드도 추가할 예정
+	constructor(post: Post, siteUser: SiteUser, voterType: VoterType) {
+		this.post = post
+		this.siteUser = siteUser
+		this.voterType = voterType
+	}
 }
