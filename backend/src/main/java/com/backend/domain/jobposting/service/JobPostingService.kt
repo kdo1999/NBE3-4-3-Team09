5ab44423684792ddee7ec326c242b8056e092a63
Kim Dong O <kdo_1999@naver.com>
2025-03-06 09:25:47 +0900

    refactor: JobPostingService 코틀린 마이그레이션

--- backend/src/main/java/com/backend/domain/jobposting/service/JobPostingService.kt
+++ backend/src/main/java/com/backend/domain/jobposting/service/JobPostingService.kt
@@ -1,18 +1,16 @@
 package com.backend.domain.jobposting.service;
 
-import com.backend.domain.jobposting.dto.JobPostingDetailResponse;
-import com.backend.domain.jobposting.dto.JobPostingPageResponse;
-import com.backend.domain.jobposting.repository.JobPostingRepository;
-import com.backend.domain.jobposting.util.JobPostingSearchCondition;
-import com.backend.global.exception.GlobalErrorCode;
-import com.backend.global.exception.GlobalException;
-import com.backend.domain.user.entity.SiteUser;
-import lombok.RequiredArgsConstructor;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
+import com.backend.domain.jobposting.dto.JobPostingDetailResponse
+import com.backend.domain.jobposting.dto.JobPostingPageResponse
+import com.backend.domain.jobposting.repository.JobPostingRepository
+import com.backend.domain.jobposting.util.JobPostingSearchCondition
+import com.backend.domain.user.entity.SiteUser
+import com.backend.global.exception.GlobalErrorCode
+import com.backend.global.exception.GlobalException
+import org.springframework.data.domain.Page
+import org.springframework.data.domain.PageRequest
+import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
 
 /**
  * JobPostingService
@@ -21,10 +19,7 @@ import org.springframework.transaction.annotation.Transactional;
  * @author Kim Dong O
  */
 @Service
-@RequiredArgsConstructor
-public class JobPostingService {
-
-	private final JobPostingRepository jobPostingRepository;
+class JobPostingService(private val jobPostingRepository: JobPostingRepository) {
 
 	/**
 	 * 채용 공고 동적 페이징 조회 메서드 입니다.
@@ -33,17 +28,14 @@ public class JobPostingService {
 	 * @return {@link Page<JobPostingPageResponse>}
 	 */
 	@Transactional(readOnly = true)
-	public Page<JobPostingPageResponse> findAll(
-		JobPostingSearchCondition jobPostingSearchCondition) {
-		int pageNum = jobPostingSearchCondition.pageNum() == null ?
-			0 : jobPostingSearchCondition.pageNum();
+	fun findAll(jobPostingSearchCondition: JobPostingSearchCondition)
+	: Page<JobPostingPageResponse> {
+		val pageNum = jobPostingSearchCondition.pageNum ?: 0
+		val pageSize = jobPostingSearchCondition.pageSize ?: 10
 
-		int pageSize = jobPostingSearchCondition.pageSize() == null ?
-			10 : jobPostingSearchCondition.pageSize();
+		val pageable = PageRequest.of(pageNum, pageSize)
 
-		Pageable pageable = PageRequest.of(pageNum, pageSize);
-
-		return jobPostingRepository.findAll(jobPostingSearchCondition, pageable);
+		return jobPostingRepository.findAll(jobPostingSearchCondition, pageable)
 	}
 
 	/**
@@ -54,19 +46,22 @@ public class JobPostingService {
 	 * @return {@link JobPostingDetailResponse}
 	 */
 	@Transactional(readOnly = true)
-	public JobPostingDetailResponse findDetailById(Long jobPostingId, Long siteUserId) {
+	fun findDetailById(jobPostingId: Long, siteUserId: Long): JobPostingDetailResponse {
 
 		return jobPostingRepository.findDetailById(jobPostingId, siteUserId)
-			.orElseThrow(() -> new GlobalException(GlobalErrorCode.JOB_POSTING_NOT_FOUND));
+			.orElseThrow { GlobalException(GlobalErrorCode.JOB_POSTING_NOT_FOUND) }
 	}
 
-	public Page<JobPostingPageResponse> findAllVoter(
-		JobPostingSearchCondition jobPostingSearchCondition, SiteUser siteUser) {
+	fun findAllVoter(
+		jobPostingSearchCondition: JobPostingSearchCondition,
+		siteUser: SiteUser
+	): Page<JobPostingPageResponse> {
 
-		Pageable pageable = PageRequest.of(
-			jobPostingSearchCondition.pageNum(), jobPostingSearchCondition.pageSize()
-		);
+		val pageNum = jobPostingSearchCondition.pageNum ?: 0
+		val pageSize = jobPostingSearchCondition.pageSize ?: 10
 
-		return jobPostingRepository.findAllVoter(jobPostingSearchCondition, siteUser.getId(), pageable);
+		val pageable = PageRequest.of(pageNum, pageSize)
+
+		return jobPostingRepository.findAllVoter(jobPostingSearchCondition, siteUser.id!!, pageable)
 	}
 }
