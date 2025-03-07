afccd3e227667719a174f1169e995cc611cdba62
Gonhub9 <ho_gok@naver.com>
2025-03-07 12:27:03 +0900

    refactor: RecruitmentUserStatus 코틀린으로 변환

--- backend/src/main/java/com/backend/domain/recruitmentUser/entity/RecruitmentUserStatus.kt
+++ backend/src/main/java/com/backend/domain/recruitmentUser/entity/RecruitmentUserStatus.kt
@@ -1,20 +1,17 @@
-package com.backend.domain.recruitmentUser.entity;
+package com.backend.domain.recruitmentUser.entity
 
-import com.backend.domain.voter.domain.VoterType;
-import com.fasterxml.jackson.annotation.JsonCreator;
-import java.util.stream.Stream;
+import com.fasterxml.jackson.annotation.JsonCreator
 
-public enum RecruitmentUserStatus {
+enum class RecruitmentUserStatus {
     APPLIED,   // 지원 완료
     ACCEPTED,  // 수락됨
     REJECTED,  // 거절됨
     NOT_APPLIED; // 지원하지 않음
 
-    @JsonCreator
-	public static RecruitmentUserStatus from(String param) {
-		return Stream.of(RecruitmentUserStatus.values())
-			.filter(recruitmentUserStatus -> recruitmentUserStatus.toString().equalsIgnoreCase(param))
-			.findFirst()
-			.orElse(null);
+    companion object{
+		@JsonCreator
+		@JvmStatic
+		fun from(param: String): RecruitmentUserStatus? =
+			entries.firstOrNull { it.name.equals(param, ignoreCase = true) }
 	}
 }
