ea88509ca0781d359c7e92c795373b247bfe1405
zelly-log <dearmyjaei@gmail.com>
2025-03-07 17:56:25 +0900

    refactor: 어노테이션 field: 누락 부분 추가

--- backend/src/main/java/com/backend/domain/post/dto/PostRequestDto.kt
+++ backend/src/main/java/com/backend/domain/post/dto/PostRequestDto.kt
@@ -8,21 +8,21 @@ import java.time.ZonedDateTime
 
 data class PostRequestDto(
 
-    @NotBlank(message = "제목을 입력해 주세요.")
+    @field:NotBlank(message = "제목을 입력해 주세요.")
     var subject: String,
 
-    @NotBlank(message = "내용을 입력해 주세요.")
+    @field:NotBlank(message = "내용을 입력해 주세요.")
     var content: String,
 
-    @NotNull(message = "카테고리를 선택해 주세요.")
+    @field:NotNull(message = "카테고리를 선택해 주세요.")
     val categoryId: Long,
 
     val jobPosting: Long ?= null, // 모집 게시판 아닐 경우 null
 
     // 모집 게시판 전용 필드
-    @Future(message = "모집 종료일은 미래 날짜여야 합니다.")
+    @field:Future(message = "모집 종료일은 미래 날짜여야 합니다.")
     var RecruitmentClosingDate: ZonedDateTime ?= null,
 
-    @Min(value = 1, message = "모집 인원은 최소 1명이어야 합니다.")
+    @field:Min(value = 1, message = "모집 인원은 최소 1명이어야 합니다.")
     var numOfApplicants: Int? = null
 )
