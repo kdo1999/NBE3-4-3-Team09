54ce25fcedc7bc40349bad64794ec59700421a1e
zelly-log <dearmyjaei@gmail.com>
2025-03-08 00:17:22 +0900

    refactor: RecruitmentPostResponse.java -> RecruitmentPostResponse.kt 변환

--- backend/src/main/java/com/backend/domain/post/dto/RecruitmentPostResponse.kt
+++ backend/src/main/java/com/backend/domain/post/dto/RecruitmentPostResponse.kt
@@ -1,39 +1,30 @@
-package com.backend.domain.post.dto;
+package com.backend.domain.post.dto
 
-import com.backend.domain.post.entity.RecruitmentStatus;
-import com.fasterxml.jackson.annotation.JsonInclude;
-import com.fasterxml.jackson.annotation.JsonInclude.Include;
-import com.querydsl.core.annotations.QueryProjection;
-import java.time.ZonedDateTime;
-import lombok.Builder;
+import com.backend.domain.post.entity.RecruitmentStatus
+import com.fasterxml.jackson.annotation.JsonInclude
+import com.fasterxml.jackson.annotation.JsonInclude.Include
+import com.querydsl.core.annotations.QueryProjection
+import java.time.ZonedDateTime
 
-@Builder
-public record RecruitmentPostResponse(
-	Long id,
-	String subject,
-	String content,
-	Long categoryId,
-	boolean isAuthor,
-	String authorName,
-	String authorImg,
-	Long voterCount,
-	boolean isVoter,
-	ZonedDateTime createdAt,
+data class RecruitmentPostResponse @QueryProjection constructor(
+    val id: Long,
+    var subject: String,
+    var content: String,
+    val categoryId: Long,
+    val isAuthor: Boolean,
+    val authorName: String,
+    val authorImg: String,
+    var voterCount: Long,
+    var isVoter: Boolean,
+    val createdAt: ZonedDateTime,
 
-	// 모집 게시판 전용 필드
-	@JsonInclude(value = Include.NON_NULL)
-	Long jobPostingId,
+    @field:JsonInclude(value = Include.NON_NULL)
+    var jobPostingId: Long,
+    @field:JsonInclude(value = Include.NON_NULL)
+    var numOfApplicants: Int,
+    @field:JsonInclude(value = Include.NON_NULL)
+    var recruitmentStatus: RecruitmentStatus,
+    @field:JsonInclude(value = Include.NON_NULL)
+    var currentAcceptedCount: Int
 
-	@JsonInclude(value = Include.NON_NULL)
-	Integer numOfApplicants,
-
-	@JsonInclude(value = Include.NON_NULL)
-	RecruitmentStatus recruitmentStatus,
-
-	@JsonInclude(value = Include.NON_NULL)
-	Integer currentAcceptedCount) {
-
-	@QueryProjection
-	public RecruitmentPostResponse {
-	}
-}
+)
