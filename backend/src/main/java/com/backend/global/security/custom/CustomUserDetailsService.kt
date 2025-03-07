30b06311553101ed1c6334ddb4b28ab26cc1e1c4
sungyeong98 <am25227903@gmail.com>
2025-03-07 11:25:23 +0900

    refactor: CustomUserDetails, CustomUserDetailsService 코틀린으로 변환 - ApiV1JobPostingController의 일부 코드 수정

---
+++ backend/src/main/java/com/backend/global/security/custom/CustomUserDetailsService.kt
@@ -0,0 +1,23 @@
+package com.backend.global.security.custom
+
+import com.backend.domain.user.entity.SiteUser
+import com.backend.domain.user.repository.UserRepository
+import org.springframework.security.core.userdetails.UserDetails
+import org.springframework.security.core.userdetails.UserDetailsService
+import org.springframework.security.core.userdetails.UsernameNotFoundException
+import org.springframework.stereotype.Service
+import java.util.*
+import java.util.function.Supplier
+
+@Service
+class CustomUserDetailsService(
+    private val userRepository: UserRepository
+) : UserDetailsService {
+    @Throws(UsernameNotFoundException::class)
+    override fun loadUserByUsername(email: String?): UserDetails {
+        val siteUser = Optional.ofNullable<SiteUser?>(userRepository!!.findByEmail(email))
+            .orElseThrow<UsernameNotFoundException?>(Supplier { UsernameNotFoundException("사용자를 찾을 수 없습니다.") })
+
+        return CustomUserDetails(siteUser)
+    }
+}
