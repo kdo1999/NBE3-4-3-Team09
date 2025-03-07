0b0a39503c3b6c0e78e4d77903fa39e8994941e5
Gonhub9 <ho_gok@naver.com>
2025-03-07 16:09:12 +0900

    refactor: AuthorRequest 리뷰 반영

--- backend/src/main/java/com/backend/domain/recruitmentUser/dto/request/AuthorRequest.kt
+++ backend/src/main/java/com/backend/domain/recruitmentUser/dto/request/AuthorRequest.kt
@@ -1,9 +1,9 @@
 package com.backend.domain.recruitmentUser.dto.request
 
-import jakarta.validation.constraints.NotNull
+import jakarta.validation.constraints.Min
 
 data class AuthorRequest(
-        @field:NotNull(message = "모집 인원 Id는 필수입니다.")
+        @field:Min(1, message = "모집 인원 Id는 필수입니다.")
         val userId: Long
 ) {
 
