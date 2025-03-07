ccf9332c5915e07ab7771af6f909cb925f2eab10
Gonhub9 <ho_gok@naver.com>
2025-03-07 15:47:33 +0900

    refactor: RecruitmentUserPostResponse 코틀린으로 변환

--- backend/src/main/java/com/backend/domain/recruitmentUser/dto/response/RecruitmentUserPostResponse.kt
+++ backend/src/main/java/com/backend/domain/recruitmentUser/dto/response/RecruitmentUserPostResponse.kt
@@ -1,12 +1,12 @@
-package com.backend.domain.recruitmentUser.dto.response;
+package com.backend.domain.recruitmentUser.dto.response
 
-import com.backend.domain.post.dto.PostPageResponse;
-import com.backend.domain.recruitmentUser.entity.RecruitmentUserStatus;
-import org.springframework.data.domain.Page;
+import com.backend.domain.post.dto.PostPageResponse
+import com.backend.domain.recruitmentUser.entity.RecruitmentUserStatus
+import org.springframework.data.domain.Page
 
-public record RecruitmentUserPostResponse(
-	RecruitmentUserStatus status,
-	Page<PostPageResponse> postPageResponses
+data class RecruitmentUserPostResponse(
+	val status: RecruitmentUserStatus,
+	val postPageResponses: Page<PostPageResponse>
 ) {
 
 }
