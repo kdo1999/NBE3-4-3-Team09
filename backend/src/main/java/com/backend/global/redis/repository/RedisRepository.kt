0287dd2d2bd811b412415e5c3fed76beec7dec46
Kim Dong O <kdo_1999@naver.com>
2025-03-08 22:53:50 +0900

    refactor: RedisRepository 코틀린 마이그레이션

--- backend/src/main/java/com/backend/global/redis/repository/RedisRepository.kt
+++ backend/src/main/java/com/backend/global/redis/repository/RedisRepository.kt
@@ -1,34 +1,32 @@
 package com.backend.global.redis.repository;
 
-import java.util.concurrent.TimeUnit;
-import lombok.RequiredArgsConstructor;
-import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.stereotype.Component;
+import org.springframework.data.redis.core.RedisTemplate
+import org.springframework.stereotype.Component
+import java.util.concurrent.TimeUnit
 
 @Component
-@RequiredArgsConstructor
-public class RedisRepository {
+class RedisRepository (private val redisTemplate: RedisTemplate<String, Any>){
 
-    private final RedisTemplate<String, Object> redisTemplate;
+     fun save(key: String, value: Any, duration: Long, timeUnit: TimeUnit) =
+         redisTemplate.opsForValue().set(key, value, duration, timeUnit)
 
-    public void save(String key, Object value, long duration, TimeUnit timeUnit) {
-        redisTemplate.opsForValue().set(key, value, duration, timeUnit);
-    }
+    /**
+     * Redis Key 값의 Value 반환
+     * @return [Any]
+     */
+    fun get(key: String): Any? = redisTemplate.opsForValue().get(key)
 
-    public Object get(String key) {
-        return redisTemplate.opsForValue().get(key);
-    }
+    fun remove(key: String) = redisTemplate.delete(key)
 
-    public void remove(String key) {
-        redisTemplate.delete(key);
-    }
+    /**
+     * Redis 키가 존재하는지 확인하는 메소드
+     * @return [Boolean] 있다면 true, 없다면 false 반환
+     */
+    fun hasKey(key: String): Boolean = redisTemplate.hasKey(key)
 
-    public boolean hasKey(String key) {
-        return redisTemplate.hasKey(key); //Redis에 key가 존재 하는지
-    }
-
-    public void save(String key, Object value) {
-        redisTemplate.opsForValue().set(key, value); //Redis에 key, value 저장
-    }
+    /**
+     * Redis에 Key, Value 저장하는 메소드
+     */
+    fun save(key: String, value: Any) = redisTemplate.opsForValue().set(key, value)
 
 }
