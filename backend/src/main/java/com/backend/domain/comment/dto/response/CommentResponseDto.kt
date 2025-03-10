1c0a0d7d71a155d8da2bd22626219d4316137679
richard3251 <paladog31@naver.com>
2025-03-10 15:47:01 +0900

    refactor: response entity kotlin 변환

---
+++ backend/src/main/java/com/backend/domain/comment/dto/response/CommentResponseDto.kt
@@ -0,0 +1,13 @@
+package com.backend.domain.comment.dto.response
+
+import java.time.ZonedDateTime
+
+data class CommentResponseDto(
+    val id: Long?,
+    val content: String,
+    val createdAt: ZonedDateTime,
+    val modifiedAt: ZonedDateTime,
+    val profileImageUrl: String,
+    val authorName: String,
+    var isAuthor: Boolean = false  // 기본값 false 설정
+)
