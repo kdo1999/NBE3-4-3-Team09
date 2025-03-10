a64951fc55d8cc1a0a993037de8e0617ca069e4e
Kim Dong O <kdo_1999@naver.com>
2025-03-10 12:12:59 +0900

    refactor: MongoDBConfig 코틀린 마이그레이션

--- backend/src/main/java/com/backend/global/config/MongoDBConfig.kt
+++ backend/src/main/java/com/backend/global/config/MongoDBConfig.kt
@@ -1,33 +1,25 @@
 package com.backend.global.config;
 
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.data.mongodb.MongoDatabaseFactory;
-import org.springframework.data.mongodb.config.EnableMongoAuditing;
-import org.springframework.data.mongodb.core.MongoTemplate;
-import org.springframework.data.mongodb.core.SimpleMongoClientDatabaseFactory;
-import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.data.mongodb.MongoDatabaseFactory
+import org.springframework.data.mongodb.config.EnableMongoAuditing
+import org.springframework.data.mongodb.core.MongoTemplate
+import org.springframework.data.mongodb.core.SimpleMongoClientDatabaseFactory
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories
 
-import lombok.extern.slf4j.Slf4j;
-
-@Slf4j
 @Configuration
-@EnableMongoRepositories(basePackages = {"com.backend.domain.chat.repository"})
+@EnableMongoRepositories(basePackages = ["com.backend.domain.chat.repository"])
 @EnableMongoAuditing
-public class MongoDBConfig {
-
-	@Value("${spring.data.mongodb.uri}")
-	private String uri;
-
-	@Bean
-	public MongoDatabaseFactory mongoDatabaseFactory() {
-		return new SimpleMongoClientDatabaseFactory(uri);
-	}
-
+class MongoDBConfig(
+	@Value("\${spring.data.mongodb.uri}")
+	val uri: String
+) {
 
 	@Bean
-	public MongoTemplate mongoTemplate() {
-		return new MongoTemplate(mongoDatabaseFactory());
-	}
+	fun mongoDatabaseFactory(): MongoDatabaseFactory = SimpleMongoClientDatabaseFactory(uri)
+
+	@Bean
+	fun mongoTemplate(): MongoTemplate = MongoTemplate(mongoDatabaseFactory())
 }
