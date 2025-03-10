9588b9b3de8f29e9c5167e8079cd9abfa541164f
Kim Dong O <kdo_1999@naver.com>
2025-03-10 12:13:22 +0900

    refactor: MailConfig 코틀린 마이그레이션

--- backend/src/main/java/com/backend/global/config/MailConfig.kt
+++ backend/src/main/java/com/backend/global/config/MailConfig.kt
@@ -1,95 +1,87 @@
 package com.backend.global.config;
 
-import com.backend.global.mail.util.EmailTemplateMaker;
-import com.backend.global.mail.util.TemplateMaker;
-import com.backend.global.mail.util.TemplateName;
-import java.util.Map;
-import java.util.Properties;
-import java.util.concurrent.ConcurrentHashMap;
-import lombok.Getter;
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.mail.javamail.JavaMailSender;
-import org.springframework.mail.javamail.JavaMailSenderImpl;
-import org.thymeleaf.spring6.SpringTemplateEngine;
-import org.thymeleaf.templateresolver.ClassLoaderTemplateResolver;
-import org.thymeleaf.templateresolver.ITemplateResolver;
+import com.backend.global.mail.util.EmailTemplateMaker
+import com.backend.global.mail.util.TemplateMaker
+import com.backend.global.mail.util.TemplateName
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.mail.javamail.JavaMailSender
+import org.springframework.mail.javamail.JavaMailSenderImpl
+import org.thymeleaf.spring6.SpringTemplateEngine
+import org.thymeleaf.templateresolver.ClassLoaderTemplateResolver
+import org.thymeleaf.templateresolver.ITemplateResolver
+import java.util.concurrent.ConcurrentHashMap
 
 @Configuration
-@Slf4j
-@Getter
-@RequiredArgsConstructor
-public class MailConfig {
+class MailConfig(
+	@Value("\${mail.host}")
+	val mailHost: String,
 
-	@Value("${mail.host}")
-	private String mailHost;
+	@Value("\${mail.port}")
+	val mailPort: Int,
 
-	@Value("${mail.port}")
-	private int mailPort;
+	@Value("\${mail.username}")
+	val mailUsername: String,
 
-	@Value("${mail.username}")
-	private String mailUsername;
+	@Value("\${mail.password}")
+	val mailPassword: String,
 
-	@Value("${mail.password}")
-	private String mailPassword;
+	@Value("\${mail.properties.mail.smtp.auth}")
+	val smtpAuth: Boolean,
 
-	@Value("${mail.properties.mail.smtp.auth}")
-	private boolean smtpAuth;
+	@Value("\${mail.properties.mail.smtp.starttls.enable}")
+	val smtpStartTlsEnable: Boolean,
 
-	@Value("${mail.properties.mail.smtp.starttls.enable}")
-	private boolean smtpStartTlsEnable;
+	@Value("\${mail.templates.path}")
+	val templatesPath: String,
 
-	@Value("${mail.templates.path}")
-	private String templatesPath;
-
-	@Value("${mail.templates.recruitment-chat}")
-	private String recruitmentChat;
-
-
+	@Value("\${mail.templates.recruitment-chat}")
+	val recruitmentChat: String
+) {
 	@Bean
-	public JavaMailSender mailSender() {
-		JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
-		mailSender.setHost(mailHost);
-		mailSender.setPort(mailPort);
-		mailSender.setUsername(mailUsername);
-		mailSender.setPassword(mailPassword);
-		Properties props = System.getProperties();
-		props.put("mail.transport.protocol", "smtp");
-		props.put("mail.smtp.starttls.enable", smtpStartTlsEnable);
-		mailSender.setJavaMailProperties(props);
-		return mailSender;
+	fun mailSender(): JavaMailSender {
+		val mailSender = JavaMailSenderImpl();
+		mailSender.host = mailHost
+		mailSender.port = mailPort
+		mailSender.username = mailUsername
+		mailSender.password = mailPassword
+		val props = System.getProperties()
+		props["mail.transport.protocol"] = "smtp"
+		props["mail.smtp.starttls.enable"] = smtpStartTlsEnable
+		mailSender.javaMailProperties = props
+		return mailSender
 	}
 
 	@Bean
-	public TemplateMaker emailTemplateMaker() {
-		Map<String, String> templateNameMap = new ConcurrentHashMap<>();
+	fun emailTemplateMaker(): TemplateMaker {
+		val templateNameMap: MutableMap<String, String> = ConcurrentHashMap()
 
 		//각 템플릿 이름 Map에 저장
-		templateNameMap.put(TemplateName.RECRUITMENT_CHAT.toString(), recruitmentChat);
+		templateNameMap[TemplateName.RECRUITMENT_CHAT.toString()] =  recruitmentChat
 
-		EmailTemplateMaker emailTemplateMaker = new EmailTemplateMaker(thymeleafTemplateEngine(),
-			templateNameMap);
+		val emailTemplateMaker = EmailTemplateMaker(
+			thymeleafTemplateEngine(),
+			templateNameMap
+		)
 
-		return emailTemplateMaker;
+		return emailTemplateMaker
 	}
 
 	@Bean
-	public SpringTemplateEngine thymeleafTemplateEngine() {
-		SpringTemplateEngine templateEngine = new SpringTemplateEngine();
-		templateEngine.setTemplateResolver(thymeleafTemplateResolver());
-		return templateEngine;
+	fun thymeleafTemplateEngine(): SpringTemplateEngine {
+		val templateEngine = SpringTemplateEngine()
+		templateEngine.setTemplateResolver(thymeleafTemplateResolver())
+		return templateEngine
 	}
 
 	@Bean
-	public ITemplateResolver thymeleafTemplateResolver() {
-		ClassLoaderTemplateResolver templateResolver = new ClassLoaderTemplateResolver();
-		templateResolver.setPrefix(templatesPath);
-		templateResolver.setSuffix(".html");
-		templateResolver.setTemplateMode("HTML");
-		templateResolver.setCharacterEncoding("UTF-8");
-		return templateResolver;
+	fun thymeleafTemplateResolver(): ITemplateResolver {
+		val templateResolver = ClassLoaderTemplateResolver()
+		templateResolver.prefix = templatesPath
+		templateResolver.suffix = ".html"
+		templateResolver.characterEncoding = "UTF-8"
+		templateResolver.setTemplateMode("HTML")
+		return templateResolver
 	}
 }
